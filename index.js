1ca3acb592d6d5a8968e85331bc528f7be758b08
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-05-08 19:06:08 -0600

    Agrgando funciones extra

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function encriptar() {
     let tituloMensaje = document.getElementById("titulo-mensaje");
     let parrafo = document.getElementById("parrafo");
     let muñeco = document.getElementById("imagen");
+    let respuesta = document.getElementById("mensaje").value;
+    let encriptado = document.getElementById("encriptado");
   
     let textoCifrado = texto
       .replace(/e/gi, "enter")
@@ -12,13 +14,12 @@ function encriptar() {
       .replace(/u/gi, "ufat");
   
     if (texto.length != 0) {
-      document.getElementById("texto").value = textoCifrado;
-      tituloMensaje.textContent = "Texto encriptado con éxito!";
-      parrafo.textContent = "";
-      muñeco.src = "img/encriptado.jpg";
+      document.getElementById("mensaje").value = textoCifrado;
+      encriptado.style.display = "none"; 
+      
     } else {
       muñeco.src = "img/muñeco.png";
-      tituloMensaje.textContent = "Excribe un mensaje para poder encriptar";
+      tituloMensaje.textContent = "Debes escribir un mensaje para poder encriptar";
       parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
     }
   }
@@ -37,14 +38,17 @@ function encriptar() {
       .replace(/ufat/gi, "u");
     
       if (texto.length != 0) {
-        document.getElementById("texto").value = textoCifrado;
-        tituloMensaje.textContent = "Texto desencriptado con éxito";
-        parrafo.textContent = "";
-        muñeco.src = "img/desencriptado.jpg";
+        document.getElementById("mensaje").value = textoCifrado;
+        encriptado.style.display = "none"; 
       } else {
         muñeco.src = "img/muñeco.png";
         tituloMensaje.textContent = "Ningún mensaje fue encontrado";
         parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
         swal("Ooops!", "Debes ingresar un texto", "warning");
       }
+  }
+
+  function copyText() {
+    navigator.clipboard.writeText(document.getElementById("mensaje").value);
+    alert("Texto copiado")
   }
